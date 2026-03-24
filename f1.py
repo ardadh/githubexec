c110e8c172285789ee088ae183b4a6dea9e64b57
rdtfyg <fdhg@gmail.com>
2026-03-24 16:14:58 +0100

    change

--- f1.py
+++ f1.py
@@ -3,4 +3,19 @@ b = float(input("enter b`s value"))
 
 sum = a + b
 
+print("Summary:", sum)a = float(input("enter a`s value"))
+b = float(input("enter b`s value"))
+
+sum = a + b
+
+print("Summary:", sum)a = float(input("enter a`s value"))
+b = float(input("enter b`s value"))
+
+sum = a + b
+
+print("Summary:", sum)a = float(input("enter a`s value"))
+b = float(input("enter b`s value"))
+
+sum = a + b
+
 print("Summary:", sum)
